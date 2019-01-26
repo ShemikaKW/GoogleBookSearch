9960cd19257b4e3c2dea6ba3e7b0a3d412893d7b
ShemikaKW <Shemikakw@gmail.com>
2019-01-26 11:04:57 -0500

    updated server.js

--- server.js
+++ server.js
@@ -11,20 +11,23 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
+// Serve up static assets (usually Heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlesearch",
- {
-   useNewUrlParser: true,
-   useCreateIndex: true
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true
   }
   );
+
 
 //Set Port
 app.listen(3001, () =>
